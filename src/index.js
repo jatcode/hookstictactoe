d8bdcd198289245644b05ac766b8231d15be7605
yokodev <yokodevmaster2015@gmail.com>
2019-05-16 18:00:25 -0600

    Fix the click event, to set it after the array setState call, * Copy the Winner function, So now the game is able to figure who is the winner and stop there.

--- src/index.js
+++ src/index.js
@@ -15,35 +15,30 @@ function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [isXnext, setIsXnext] = useState(true)
   const [gameOver, setGameOver] = useState(false)
-  const [clicks, setClicks] = useState(0)
   
   const handleClick = (i)=>{
-    if (clicks === 9 ){
-      setSquares(Array(9).fill(null))
-      setIsXnext(true)
-      setClicks(0)
-      console.log('gameOver BABY!!!!: ')
-    }
-    console.log(isXnext)
+    console.log(`isXnext: ${isXnext}`)
     let nextSquares
-    setClicks(clicks+ 1)
-    setIsXnext(!isXnext)
-    console.log('clicks: ', clicks,'isXnext', isXnext)
-    if (clicks > 8 ){
-      setGameOver(true)
-      console.log('gameOver: ', gameOver)
-    }else{
-      nextSquares = [...squares]
-      isXnext ? nextSquares[i] = 'x' : nextSquares[i] = 'o' 
-      setSquares(nextSquares)
+    nextSquares = [...squares]
+    if(calculateWinner(squares) || squares[i]){
+      return
     }
+    nextSquares[i] = isXnext ? 'x' : 'o' 
+    setIsXnext(!isXnext)
+    setSquares(nextSquares)
   }
 
   const renderSquare =(i)=> {
     return <Square value={squares[i]}  onClick={()=>{handleClick(i)}} />;
   }
 
-    const status = 'Next player: X';
+  const winner = calculateWinner(squares)
+  let status
+  if(winner) {
+    status = `Winner: ${winner}`
+  }else {
+    status = `Next player: ${isXnext ? 'X' : 'O'}`;
+  }
 
   return (
     <div>
@@ -89,3 +84,25 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+//helper function
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+//helper function
